f393e2cb9ae1499073afd1c9854414fd2c747d9f
Thales Gomes <thalessud@gmail.com>
2021-03-20 20:19:15 +0000

    refactor App as class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(pos) => console.log(pos),
-		(err) => console.log(err)
-	);
-
-	return <div>Latitude: </div>;
-};
+class App extends React.Component {
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			(pos) => console.log(pos),
+			(err) => console.log(err)
+		);
+		return <div>Latitude: </div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
