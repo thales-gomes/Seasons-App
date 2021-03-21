4fa07da1258c08ede125a5952490284b76787ac5
Thales Gomes <thalessud@gmail.com>
2021-03-21 11:03:01 +0000

    Conditional rendering and automatic latitude  update

--- src/index.js
+++ src/index.js
@@ -4,17 +4,26 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { lat: null }
+		this.state = { lat: 'loading', errMessage: null }
+	}
+
+	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			pos => { this.setState({ lat: pos.coords.latitude}) },
-			err => err
+			pos => { this.setState({ lat: pos.coords.latitude }) },
+			err => { this.setState({ errMessage: err.message }) }
 		)
 	}
 
 	render() {
-		const { lat } = this.state;
+		const { lat, errMessage } = this.state;
+		
+		const display =	(!errMessage)?
+			(<div>Latitude: {lat} </div>) :
+			(<div>Error: {errMessage} </div>)
 
-		return <div>Latitude: {lat} </div>;
+		return (
+			display
+		)
 	}
 }
 
