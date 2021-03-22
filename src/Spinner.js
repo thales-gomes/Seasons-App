3847c787bf8ebd151b8408c37ae80ee090917c7c
Thales Gomes <thalessud@gmail.com>
2021-03-22 10:07:12 +0000

    renderContent function added to avoid conditional blocks on render

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Spinner = (props) => {
-  return (
-    <div className="ui active dimmer">
-      <div className="ui big text loader">{props.message}</div>
-    </div>
-  )
-}
+	return (
+		<div className="ui active dimmer">
+			<div className="ui big text loader">{props.message}</div>
+		</div>
+	);
+};
 
 Spinner.defaultProps = {
-  message: 'Loading...'
+	message: "Loading...",
 };
 
 export default Spinner;
--- src/index.js
+++ src/index.js
@@ -4,27 +4,31 @@ import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 
 class App extends React.Component {
-	state = { lat: null, errMessage: null }
+	state = { lat: null, errMessage: null };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			pos => this.setState({ lat: pos.coords.latitude }),
-			err => this.setState({ errMessage: err.message })
-		)
+			(pos) => this.setState({ lat: pos.coords.latitude }),
+			(err) => this.setState({ errMessage: err.message })
+		);
+	}
+
+	renderContent() {
+		const { lat, errMessage } = this.state;
+
+		let display = <Spinner message="Please accept location request" />;
+
+		if (errMessage && !lat) {
+			display = <div>Error: {errMessage} </div>;
+		}
+		if (!errMessage && lat) {
+			display = <SeasonDisplay lat={lat} />;
+		}
+		return display;
 	}
 
 	render() {
-		const { lat, errMessage } = this.state;
-
-		let display = <Spinner message="Please accept location request"/>;
-
-		if (errMessage && !lat) {
-			display = <div>Error: {errMessage} </div>
-		}
-		if (!errMessage && lat) {
-			display = (<SeasonDisplay lat={lat} />)
-		}
-		return display
+		return <div className="border red">{this.renderContent()}</div>;
 	}
 }
 
